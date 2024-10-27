fee5f518db58a0d0f721ac25a503ddabbfb51f45
hayro45 <hayroxgitar47@gmail.com>
2024-10-27 20:18:22 +0300

    Feat: Established CQRS & Onion Architecture Design Pattern for Order Microservice
    Add order handling projects and APIs to MultiShop solution
    
    Introduced new projects for order handling in the MultiShop solution, including domain, application, persistence, and web API layers. Key changes:
    
    - Created `MultiShop.Order.Application` targeting .NET 6.0 with command and query handlers for address and order detail operations.
    - Added domain entities `Address`, `OrderDetail`, and `Ordering`.
    - Defined `IRepository` interface for data access abstraction.
    - Created `MultiShop.Order.Persistance` with `OrderContext` for EF Core and a generic `Repository<T>`.
    - Configured `MultiShop.Order.WebApi` with controllers for `Address` and `OrderDetail` entities, Swagger setup, and launch settings.
    - Updated `MultiShop.sln` to include new projects.

---
+++ Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Handlers/OrderDetailHandlers/GetOrderDetailByIdQueryHandler.cs
@@ -0,0 +1,34 @@
+﻿using MultiShop.Order.Application.Features.CQRS.Queries.AddressQueries;
+using MultiShop.Order.Application.Features.CQRS.Queries.OrderDetailQueries;
+using MultiShop.Order.Application.Features.CQRS.Results.AddressResults;
+using MultiShop.Order.Application.Features.CQRS.Results.OrderDetailResults;
+using MultiShop.Order.Application.Interfaces;
+using MultiShop.Order.Domain.Entities;
+
+namespace MultiShop.Order.Application.Features.CQRS.Handlers.OrderDetailHandlers
+{
+    public class GetOrderDetailByIdQueryHandler
+    {
+        private readonly IRepository<OrderDetail> _repository;
+
+        public GetOrderDetailByIdQueryHandler(IRepository<OrderDetail> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<GetOrderDetailByIdQueryResult> Handle(GetOrderDetailByIdQuery query)
+        {
+            var result = await _repository.GetByIdAsync(query.Id);
+            return new GetOrderDetailByIdQueryResult
+            {
+                OrderDetailId = result.OrderDetailId,
+                ProductId = result.ProductId,
+                ProductName = result.ProductName,
+                ProductPrice = result.ProductPrice,
+                ProductAmount = result.ProductAmount,
+                ProductTotalPrice = result.ProductTotalPrice,
+                OrderinId = result.OrderingId
+            };
+        }
+    }
+}
